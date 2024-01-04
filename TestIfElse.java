cff399a7625d5ae9d69a4af0304d5f8034f1c8f8
LAPTOP-S1KE91NG\咩咩 <2395052233@qq.com>
2024-01-04 21:53:13 +0800

    上传

---
+++ TestIfElse.java
@@ -0,0 +1,12 @@
+public class TestIfElse {
+	public static void main(String[] args) {
+		int x = 30;
+		if(x == 10) {
+			System.out.println("x的值是");
+		} else if (x == 20) {
+			System.out.println("x的值是20");
+		} else if (x == 30) {
+			System.out.println("x的值是30");
+		}
+	}
+}
---
+++ TestSwichCase.java
@@ -0,0 +1,26 @@
+public class TestSwichCase{
+	public static void main(String[] args) {
+		char grade = 'E';
+		switch(grade)
+		{
+		case 'A' :
+		System.out.println("优秀");
+		break;
+		case 'B' :
+		case 'C' :
+		System.out.println("良好");
+		case 'D' :
+		System.out.println("及格");
+		break;
+		default:
+		System.out.println("未知");
+		}System.out.println("等级是" + grade);
+		int x = 0;
+		x = x++;
+		System.out.println(x);
+		int y = 0;
+		y = ++y;
+		System.out.println(y);
+	}
+	
+}
